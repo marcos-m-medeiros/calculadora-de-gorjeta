75adb22ddad993bc6c2d14a218d66fc571db47dd
Marcos Medeiros <marcosbourdon@hotmail.com>
2023-09-11 10:31:34 -0300

    Portuguese to English
    Translated all the code to English.

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-// Acessa os elementos HTML
+// Access HTML elements
 const billInput = document.querySelector("#bill");
 const ratePercentageSelect = document.querySelector("#ratePercentage");
 const numPeopleInput = document.querySelector("#numPeople");
 const tipResult = document.querySelector("#tip");
 
 function calculate() {
-    // Obtém os valores dos campos de entrada
+    // Gets the values of the input fields
     const bill = parseFloat(billInput.value);
     const ratePercentage = parseInt(ratePercentageSelect.value);
     const numPeople = parseInt(numPeopleInput.value);
 
-    // Verifica se os valores são válidos
+    // Checks if the values are valid
     if (!isNaN(bill) && !isNaN(ratePercentage) && !isNaN(numPeople) && numPeople > 0) {
         // Calcula a gorjeta total
         const tipTotal = (bill * ratePercentage) / 100;
 
-        // Calcula a gorjeta por pessoa
+        // Calculate tip per person
         const tipPerPerson = tipTotal / numPeople;
 
-        // Exibe o resultado no elemento HTML
+        // Display the result in the HTML element
         tipResult.innerHTML = `Gorjeta por pessoa: R$ ${tipPerPerson.toFixed(2)}`
     } else {
-        // Exibe uma mensagem de erro se os valores não forem válidos
+        // Displays an error message if values are not valid
         document.querySelector("#tipResult")
         tipResult.innerHTML = "Valores inválidos.";
     }
